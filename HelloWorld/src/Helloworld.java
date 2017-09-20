3865388a60fea737736066d1bbeaa073374d5314
Ehret.Dana <Ehret.Dana@SK-D8CB8A509AF5.2631.skool.net>
2017-09-20 16:30:02 +0200

    Collatz Anfang

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -2,8 +2,18 @@
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
-
-	}
+		int n = 19;
+		
+		 if(n%2==0)
+		 {
+			n=n/2;
+		 }
+		 else
+		 {
+			n=3*n+1;
+		 }
+		 System.out.println(n);
+	 }
+		 
 
 }
